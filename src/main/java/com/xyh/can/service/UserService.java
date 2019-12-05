76d2a28f85adbefeef1ac10107c443fbbbbe1a96
Huangalong-GitHub <2312666570@qq.com>
2019-12-05 15:08:43 +0800

    2019.12.5.14.59

--- src/main/java/com/xyh/can/service/UserService.java
+++ src/main/java/com/xyh/can/service/UserService.java
@@ -1,10 +1,14 @@
 package com.xyh.can.service;
 
 import com.xyh.can.entity.User;
+import com.xyh.can.vo.UserVo;
 
 import java.util.List;
 
 public interface UserService {
-    public List<User> fingpage();
-
+    public List<User> fingpage(UserVo uservo);//用戶分页
+    public Integer Count(UserVo uservo);//查询总条数
+    public Integer addUser(User user);//添加用户
+    public Integer updateUSer(User user);//修改用户
+    public Integer delUser(Integer u_id);//删除用户
 }
